716f76279c49c224ef9d019b9e871a899e651985
soham6969 <87664652+soham6969@users.noreply.github.com>
2021-08-05 22:39:46 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,10 @@
+console.log("Day3")
+function doSomething(){
+
+    let pname=document.querySelector("#name").value;
+    let page=document.querySelector("#age").value;
+    document.querySelector("#space1").innerHTML=`<h1>${pname}</h1>`;
+    document.querySelector("#space2").innerHTML=`<h2>${page}</h2>`;
+
+}
+
